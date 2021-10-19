755182dab744269d20b1d1d2c0ba9aaf6d979efa
mingjun97 <my@lyric.today>
2021-10-19 01:48:29 -0700

    Feature: use redis as lru cache

--- utils/cache/__init__.py
+++ utils/cache/__init__.py
@@ -1,11 +1,16 @@
 
-from functools import lru_cache
+import redis
 
 from main import config
 
-from .timed_cache import timed_lru_cache
 
 caches = []
+
+
+client = redis.StrictRedis(**config.cache.redis)
+from redis_lru import RedisLRU
+redis_lru_cache = RedisLRU(client)
+# from .redis_lru_cache import lru_cache_redis
 
 def gg_cache(func=None, cache_type='lru_cache', maxsize=None):
     # print("GG Cache:", func.__name__, func.__module__)
@@ -17,16 +22,29 @@ def gg_cache(func=None, cache_type='lru_cache', maxsize=None):
                 print(f"Use maxsize from config for{func_path}:", maxsize)
             except:
                 pass
-        @lru_cache(maxsize=maxsize)
+        @redis_lru_cache
         def wrapper(*args, **kwargs):
             return func(*args, **kwargs)
         caches.append(wrapper)
         return wrapper
     if cache_type == 'timed_cache':
-       return timed_lru_cache
+        def wrapper(func=None, *args, **kwargs):
+            func_path = f"{func.__module__}.{func.__name__}"
+            try:
+                _config = eval(f"config.cache.{func_path}")
+                print(f"Use config from config for{func_path}:", _config)
+            except:
+                pass
+            @redis_lru_cache(ttl=_config.ttl)
+            def _wrapped(*args, **kwargs):
+                return func(*args, **kwargs)
+            return _wrapped
+        if func is not None:
+            return wrapper(func)
+        else:
+            return wrapper
 
 
 def clear_cache():
-    for cache in caches:
-        cache.cache_clear()
+    redis_lru_cache.clear_all_cache()
 
