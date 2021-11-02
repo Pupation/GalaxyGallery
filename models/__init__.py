2068961e4f0aed77fd9bd7cf1f65eee72be26415
mingjun97 <my@lyric.today>
2021-11-01 23:24:49 -0700

    :bug: Fix error message when initializing the sql db

--- models/__init__.py
+++ models/__init__.py
@@ -1,24 +1,35 @@
+from pydantic import BaseModel
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import Table
 
-Base:Table = declarative_base()
+Base: Table = declarative_base()
 
-def create_all():
+
+async def create_all():
     from utils.connection.sql.db import engine
     from utils.connection.sql.db import get_sqldb
-    Base.metadata.create_all(bind=engine)
+    # Base.metadata.create_all(bind=engine)
 
     from .user.role import Role
-    for db in get_sqldb():
-        db.add(Role(role_name='unconfirmed', admin=False, permissions=1))
-        db.add(Role(role_name='admin', admin=True, permissions=0))
-        db.add(Role(role_name='newbie', admin=False, permissions=1))
-        db.add(Role(role_name='{customize}', admin=False, permissions=0))
-        db.commit()
-        db.close()
+    async with engine.begin() as db:
+        await db.run_sync(Base.metadata.create_all)
 
+    async with AsyncSession(engine) as db:
+        try:
+            db.add_all([
+                Role(role_name='unconfirmed', admin=False, permissions=1),
+                Role(role_name='admin', admin=True, permissions=0),
+                Role(role_name='newbie', admin=False, permissions=1),
+                Role(role_name='{customize}', admin=False, permissions=0)
+            ])
+            await db.commit()
+        except:
+            pass
+        finally:
+            await db.close()
+    return {'ok': 1}
 
-from pydantic import BaseModel
 
 class GeneralResponse(BaseModel):
     ok: int = 1
