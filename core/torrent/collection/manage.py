f52ca8f3950076af9ca49804c04c9fe267f7acdd
mingjun97 <my@lyric.today>
2021-11-13 23:50:38 -0800

    :sparkles: Impl route for deleting collection

--- core/torrent/collection/manage.py
+++ core/torrent/collection/manage.py
@@ -2,11 +2,12 @@ from . import collection_router
 
 from fastapi import Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 
 from models.user.user import User
 from models.user.auth import current_active_user
 from models.user.role import Permission
-from models.torrent.collection import CollectionSource, Collection
+from models.torrent.collection import CollectionItems, CollectionSource, Collection
 from models.forms import CollectionForm
 
 from utils.connection.sql.db import get_sqldb
@@ -35,5 +36,15 @@ async def update_collection(cid:int, user: User = Depends(current_active_user)):
     pass
 
 @collection_router.delete('/{cid:int}')
-async def delete_collection(cid:int, user: User = Depends(current_active_user)):
-    pass
+async def delete_collection(cid:int, user: User = Depends(current_active_user), db: AsyncSession= Depends(get_sqldb)):
+    sql = select(Collection).where(Collection.id == cid)
+    ret, = (await db.execute(sql)).one_or_none()
+    if ret is None:
+        raise HTTPException(404, 'Not found')
+    if ret.owner_id != user.id or ret.source == CollectionSource.official or ret.source == CollectionSource.group:
+        if not user.has_permission(Permission.MANAGE_COLLECTION):
+            raise HTTPException(403, "You do not have permission to create an official collection")
+    sql = CollectionItems.__table__.delete().where(CollectionItems.cid == cid)
+    await db.execute(sql)
+    await db.delete(ret)
+    return {'ok': 1}
